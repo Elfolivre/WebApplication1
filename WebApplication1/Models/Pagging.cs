ed838e48e83f7a676f952cea4a480d8f980ffce8
Elfolivre <78561081+Elfolivre@users.noreply.github.com>
2021-02-05 00:38:42 -0300

    Update Pagging.cs

--- WebApplication1/Models/Pagging.cs
+++ WebApplication1/Models/Pagging.cs
@@ -7,8 +7,10 @@ namespace WebApplication1.Models
 { 
     [HttpPost]
     public class Pagging
-    {
+    {   
+        [Route("receber/{PageSize;string}")]
         public int PageSize { set; }
+        [Route("receber/CurrentPage;string")]
         public int CurrentPage { set; }
     }
 }
